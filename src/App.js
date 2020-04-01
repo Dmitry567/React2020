4e50824603bae147fd8fb97ef9b026f912d49a2b
dfavo <favorskyd@gmail.com>
2020-03-31 19:02:13 -0700

    Add new changes to Car and App files

--- src/App.js
+++ src/App.js
@@ -5,17 +5,32 @@ import Car from './Car/Car';
 // In E6 JS we have to use className
 // We have to use root element to wrap up other elements EX <div>
 class App extends Component {
+
+    state = {
+      cars: [
+          {name: 'Ford', year: 2018},
+          {name: 'Audi', year: 2016},
+          {name: 'Mazda', year: 2010}
+      ],
+        pageTitle: 'React components'
+    }
+
     render() {
         const divStyle = {
            textAlign: 'center'
         }
+
+        const cars = this.state.cars
+
         return (
             <div style={divStyle}>
-               <h1>Hello World!</h1>
+               <h1>{this.state.pageTitle}</h1>
 
 
-                <Car />
-                <Car />
+                <Car name={cars[0].name} year={cars[0].year}/>
+                <Car name={cars[1].name} year={cars[1].year}/>
+                <Car name={cars[2].name} year={cars[2].year}/>
+
            </div>
        );
     }
@@ -36,3 +51,4 @@ export default App;
  */
 //           )
 //         )
+// State describe the condition of the component
